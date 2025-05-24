530aeacb108891be50838d798636d70ff50cb9f5
Nintron <nintron@sent.com>
2025-05-24 10:51:48 -0400

    Organize data.go a *tiny* bit

--- web/templates/data.go
+++ web/templates/data.go
@@ -1,10 +1,23 @@
 package templates
 
+// layouts/primary
+
 type DataLayoutPrimary struct {
 	NavData    DataComponentNav
 	FooterData DataComponentFooter
 	PageData   any
 }
+type DataComponentNav struct{}
+
+type DataComponentFooter struct {
+	Links []DataComponentFooterLink
+}
+type DataComponentFooterLink struct {
+	Href string
+	Text string
+}
+
+// layouts/app
 
 type DataLayoutApp struct {
 	NavData     DataComponentAppNav
@@ -18,32 +31,23 @@ type DataComponentAppNav struct {
 	Username     string
 }
 
-type DataComponentNav struct{}
-
-type DataComponentFooter struct {
-	Links []DataComponentFooterLink
-}
-
-type DataComponentFooterLink struct {
-	Href string
-	Text string
-}
+// pages/wallet-home
 
 type DataPageWalletHomepage struct {
 	WalletAddr   string
 	SteloSummary DataComponentSteloSummary
 }
-
 type DataComponentSteloSummary struct {
 	FeaturedAsset    string
 	FeaturedAssetQty float64
 }
 
+// pages/homepage
+
 type DataPageHomepage struct {
 	User      bool
 	InfoCards []DataPageHomepageInfoCard
 }
-
 type DataPageHomepageInfoCard struct {
 	Title string
 	Body  string
